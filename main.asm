56ce1a17b19fc0dbf13c8e0f202b5bc8fc5fd8d4
Paulo Bruno <paulobruno@alu.ufc.br>
2020-12-22 15:03:25 -0300

    Add initialization functions

--- main.asm
+++ main.asm
@@ -7,6 +7,32 @@ MAX_ROWS    EQU     $12
 MAX_COLS    EQU     $14
 
 
+SECTION "Entry", ROM0[$100]
+    nop
+    jp Start
+
+REPT $150 - $104
+    db 0
+ENDR
+
+
+SECTION "Main", ROM0
+
+Start:
+    ; initialization
+    call waitVBlank
+    call turnOffLcd
+    call setDefaultPalette
+
+    ld b, $00
+    call fillScreen
+
+    ld hl, $8800
+    ld de, BlackTileStart
+    ld bc, BlackTileEnd - BlackTileStart
+    call copyToMemory
+
+    call turnOnLcd
 
     call resetTilePosition
 
@@ -67,6 +93,61 @@ MAX_COLS    EQU     $14
     jp goToNextTile
 
 
+SECTION "Sprites", ROM0
+
+BlackTileStart:
+    db $00, $00, $00, $00
+    db $00, $00, $00, $00
+    db $00, $00, $00, $00
+    db $00, $00, $00, $00
+BlackTileEnd:
+
+
+SECTION "Functions", ROM0
+
+waitVBlank:
+    ld a, [rLY]
+    cp 144
+    jr c, waitVBlank
+    ret
+
+turnOffLcd:
+    xor a ; ld a, 0
+    ld [rLCDC], a
+    ret
+    
+turnOnLcd:
+    ld a, %10010011
+    ld [rLCDC], a
+    ret
+
+setDefaultPalette:
+    ld a, %11100100
+    ld [rBGP], a
+    ld [rOBP0], a
+    ret
+
+copyToMemory:
+    ld a, [de]
+    ld [hli], a ; ld [hl], a ; inc hl
+    inc de
+    dec bc
+    ld a, b
+    or c
+    jr nz, copyToMemory
+    ret
+
+; fill the screen with the tile at address in register b
+fillScreen:
+    ld hl, vBGMap0
+.clear
+    ld a, b
+    ld [hli], a
+    ld a, h
+    cp $9C ; screen ends at $9C00
+    jr nz, .clear
+    ret
+    
 countNeighbors:
     xor a
     ld b, a
